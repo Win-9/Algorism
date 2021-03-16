ea6506e6f99e98f32218d32740be72f72326e0a4
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-03-16 23:45:07 +0900

    Commit

--- src/por/Main.java
+++ src/por/Main.java
@@ -6,79 +6,66 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-
 class Main {
-	public static void main(String[]args) throws IOException{
-		Solution s=new Solution();
-		Solution1 s1=new Solution1();
-		int n=5;
-		int[]arr= {2,3,4};
-		int[]arr2={1,2,3};
-		int[]a= {10,1,10,2,1};
-		System.out.println(s.solution());
-		System.out.println(s.solution(5,arr,arr2));
+	  public static void main(String[] args) {
+	    System.out.println("Hello world!");
+	  }
 	}
-}
-class Solution1{
-	public int solution() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[]str=br.readLine().split("");
-		int end=Integer.parseInt(str[2]);
-		int start=Integer.parseInt(str[1]);
-		int up=Integer.parseInt(str[3]);int down=Integer.parseInt(str[4]);
-		LinkedList<Integer>[]list=new LinkedList[2];
-		for(int i=0;i<2;i++) {
-			list[i]=new LinkedList<Integer>();
-		}
-		dfs();
+	class Solution{
+	  static int[]x_loc={1,-1,0,0};
+	  static int[]y_loc={0,0,1,-1};
+	  static int number;
+	  public void solution() throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    ArrayList<Integer>list=new ArrayList<>();
+	    number=Integer.parseInt(br.readLine());
+	    int [][]map=new int[number][number];
+	    boolean[][]visited=new boolean[number][number];
+	    int count=0;
+	    String[]str=br.readLine().split("");
+	    for(int i=0;i<number;i++){
+	      for(int j=0;j<number;j++){
+	        map[i][j]=Integer.parseInt(str[j]);
+	      }
+	    }
+	    for(int i=0;i<number;i++){
+	      for(int j=0;j<number;j++){
+	        if(!visited[i][j]){
+	          count=bfs(visited,map,i,j,count);
+	        }
+	      }
+	    }
+	  }
+	  public int bfs(boolean[][]visited,int[][]map,int i,int j,int count){
+	    Queue<Location>queue=new LinkedList<>();
+	    queue.add(new Location(i, j));
+	    visited[i][j]=true;
+	    while(queue.size()!=0){
+	      Location loc=queue.poll();
+	      for(int k=0;k<4;k++){
+	        int ex1=loc.x+x_loc[k];
+	        int ex2=loc.y+y_loc[k];
+	        if(check(visited,ex1,ex2,map,count)){
+	          queue.add(new Location(ex1,ex2));
+	        }
+	      }
+	    }
+	    return count;
+	  }
+	  public boolean check(boolean[][]visited,int ex1,int ex2,int[][]map,int count){
+	    if((ex1>=0 && ex1<number)&&(ex2>=0 && ex2<number)&&!visited[ex1][ex2]&& map[ex1][ex2]==1){
+	      visited[ex1][ex2]=true;
+	      count++;
+	      return true;
+	    }
+	    return false;
+	  }
 	}
-	public void dfs() {
-		Queue<Integer>queue=new LinkedList<>();
-		while(queue.size()!=0) {
-			int a=queue.poll();
-		}
+	class Location{
+	  int x;
+	  int y;
+	  Location(int x,int y){
+	    this.x=x;
+	    this.y=y;
+	  }
 	}
-}
-class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer=(n-lost.length);
-        boolean[]visited=new boolean[reserve.length];
-        boolean[]visited1=new boolean[lost.length];
-        for(int i=0;i<lost.length;i++){
-            int ex1=lost[i];
-            for(int j=0;j<reserve.length;j++){
-            	if(ex1==reserve[j]) {
-            		visited[j]=true;
-            		visited1[i]=true;
-            		System.out.println("same:"+ex1);
-            		++answer;
-            		break;
-            	}
-            }
-        }
-        for(int i=0;i<lost.length;i++){
-            int ex1=lost[i];
-            int a=ex1-1;
-            int b=ex1+1;
-            for(int j=0;j<reserve.length;j++){
-                if((b==reserve[j] && !visited[j] && !visited1[i])){
-                	System.out.println("ex1:"+ex1);
-                	System.out.println("b:"+b);
-                    visited[j]=true;
-                    visited1[i]=true;
-                    ++answer;
-                    break;
-                }
-                else if((a==reserve[j] && !visited[j] && !visited1[i])) {
-                	System.out.println("ex1:"+ex1);
-                	System.out.println("a:"+a);
-                    visited[j]=true;
-                    visited1[1]=true;
-                    ++answer;
-                    break;
-                }
-            }
-        }
-        return answer;
-    }
-}
