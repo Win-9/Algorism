bf024c9ab00a2eddf79f069d56f9f36b988936c0
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-03-17 00:02:22 +0900

    Update main.java

--- BOJ/단지번호붙이기/main.java
+++ BOJ/단지번호붙이기/main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 class Main {
@@ -16,30 +17,41 @@ class Solution{
 	  static int[]x_loc={1,-1,0,0};
 	  static int[]y_loc={0,0,1,-1};
 	  static int number;
+	  static int complex=0;
+	  static int count=0;
 	  public void solution() throws IOException{
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    ArrayList<Integer>list=new ArrayList<>();
 	    number=Integer.parseInt(br.readLine());
+	    int complex=0;
 	    int [][]map=new int[number][number];
 	    boolean[][]visited=new boolean[number][number];
 	    int count=0;
-	    String[]str=br.readLine().split("");
 	    for(int i=0;i<number;i++){
+	      String[]str=br.readLine().split("");
 	      for(int j=0;j<number;j++){
 	        map[i][j]=Integer.parseInt(str[j]);
 	      }
 	    }
 	    for(int i=0;i<number;i++){
 	      for(int j=0;j<number;j++){
-	        if(!visited[i][j]){
-	          count=bfs(visited,map,i,j,count);
-	          
+	        if(!visited[i][j] && map[i][j]==1){
+	          count=bfs(visited,map,i,j);
+	          System.out.println("count:"+count);
+	          list.add(count);
+	          count=0;
 	        }
 	      }
 	    }
+	    System.out.println(complex);
+	    Collections.sort(list);
+	    for(int i=0;i<list.size();i++) {
+	    	System.out.println(list.get(i));
+	    }
 	  }
-	  public int bfs(boolean[][]visited,int[][]map,int i,int j,int count){
+	  public int bfs(boolean[][]visited,int[][]map,int i,int j){
 	    Queue<Location>queue=new LinkedList<>();
+	    complex++;
 	    queue.add(new Location(i, j));
 	    visited[i][j]=true;
 	    while(queue.size()!=0){
@@ -47,14 +59,15 @@ class Solution{
 	      for(int k=0;k<4;k++){
 	        int ex1=loc.x+x_loc[k];
 	        int ex2=loc.y+y_loc[k];
-	        if(check(visited,ex1,ex2,map,count)){
+	        if(check(visited,ex1,ex2,map)){
 	          queue.add(new Location(ex1,ex2));
 	        }
 	      }
 	    }
+	    System.out.println("countt:"+count);
 	    return count;
 	  }
-	  public boolean check(boolean[][]visited,int ex1,int ex2,int[][]map,int count){
+	  public boolean check(boolean[][]visited,int ex1,int ex2,int[][]map){
 	    if((ex1>=0 && ex1<number)&&(ex2>=0 && ex2<number)&&!visited[ex1][ex2]&& map[ex1][ex2]==1){
 	      visited[ex1][ex2]=true;
 	      count++;
