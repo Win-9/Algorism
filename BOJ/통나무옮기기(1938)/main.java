c80b2cb32df5e82fdcedb732c45821e3c448e6dc
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-03-25 21:21:06 +0900

    Update main.java

--- BOJ/통나무옮기기(1938)/main.java
+++ BOJ/통나무옮기기(1938)/main.java
@@ -15,8 +15,12 @@ class Main{
 	  static int[]case2_y_loc={0,0,2,-2};
 	  static int number;
 	  static int second=0;
+	  static boolean contact=false;
 	  static Queue<Location>queue=new LinkedList<>();
 	  public static void main(String[]args) throws Exception{
+		Location[]start=new Location[2];
+		Location[]end=new Location[2];
+		int count=0;int count2=0;
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String[] str=br.readLine().split(" ");
 	    number=Integer.parseInt(str[0]);
@@ -26,31 +30,52 @@ class Main{
 	      str=br.readLine().split(" ");
 	      for(int j=0;j<number;j++){
 	    	  map[i][j]=str[j];
+	    	  if(map[i][j].equals("B")) {
+	    		  start[count]=new Location(i,j);
+	    		  ++count;
+	    	  }
+	    	  else if(map[i][j].equals("E")) {
+	    		  start[count2]=new Location(i,j);
+	    		  ++count2;
+	    	  }
 	      }
 	    }
-	    bfs(visited,map,);
+	    if(start[1].x==start[0].x-1) {
+	    	count=bfs(visited,map,start[1].x,start[1].y,true,end[1].x,end[1].y);
+	    }
+	    else {
+	    	count=bfs(visited,map,start[1].x,start[1].y,false,end[1].x,end[1].y);
+	    }
+	    System.out.println(count);
+	    
 	  }
-	  static public void bfs(boolean[][]visited,String[][]map,int i,int j){
+	  static public int bfs(boolean[][]visited,String[][]map,int i,int j,
+			  boolean state,int end1,int end2){
 		  visited[i][j]=true;
+		  Location loc=null;
+		  queue.add(new Location(i,j,state,0));
 		  while(queue.size()!=0) {
-			  Location loc=queue.poll();
+			  loc=queue.poll();
+			  if(loc.x==end1 && loc.y==end2) {
+				  return loc.count;
+			  }
 			  if(turn(loc,map)) {
 				  loc.state=!loc.state;
-				  queue.add(new Location(loc.x, loc.y,loc.state));
+				  queue.add(new Location(loc.x, loc.y,loc.state,loc.count));
 			  }
 			  if(loc.state==true) {
 				  for(int k=0;k<4;k++) {
 					  int ex1=loc.x+case1_x_loc[k];
 					  int ex2=loc.x+case1_y_loc[k];
-					  if(check(visited,ex1,ex2)) {
+					  if(check(visited,ex1,ex2,map)) {
 						  if(case1_x_loc[k]<0)
 						  {
 							  visited[ex1+1][ex2]=true;
-							  queue.add(new Location(ex1+1, ex2,loc.state));
+							  queue.add(new Location(ex1+1, ex2,loc.state,loc.count+1));
 						  }
 						  else {
 							  visited[ex1-1][ex2]=true;
-							  queue.add(new Location(ex1-1, ex2,loc.state));
+							  queue.add(new Location(ex1-1, ex2,loc.state,loc.count+1));
 						  }
 					  }
 				  }
@@ -59,23 +84,25 @@ class Main{
 				  for(int k=0;k<4;k++) {
 					  int ex1=loc.x+case2_x_loc[k];
 					  int ex2=loc.x+case2_y_loc[k];
-					  if(check(visited,ex1,ex2)) {
+					  if(check(visited,ex1,ex2,map)) {
 						  if(case2_x_loc[k]<0)
 						  {
 							  visited[ex1+1][ex2]=true;
-							  queue.add(new Location(ex1+1, ex2,loc.state));
+							  queue.add(new Location(ex1+1, ex2,loc.state,loc.count+1));
 						  }
 						  else {
 							  visited[ex1-1][ex2]=true;
-							  queue.add(new Location(ex1-1, ex2,loc.state));
+							  queue.add(new Location(ex1-1, ex2,loc.state,loc.count+1));
 						  }
 					  }
 				  }
 			  }
 		  }
+		  return 0;
 	  }
-	  static public boolean check(boolean[][]visited,int ex1,int ex2){
-	    if((ex1>0 && ex1<=number)&&(ex2>0 && ex2<=number)&&!visited[ex1][ex2]){
+	  static public boolean check(boolean[][]visited,int ex1,int ex2,String[][]map){
+	    if((ex1>=0 && ex1<=number)&&(ex2>=0 && ex2<=number)&&!visited[ex1][ex2]
+	    		&& !map[ex1][ex2].equals("1")){
 	      return true;
 	    }
 	    return false;
@@ -109,10 +136,16 @@ class Location{
 	int x;
 	int y;
 	boolean state=true;//'-'모양
-	Location(int x,int y,boolean state){
+	int count;
+	Location(int x,int y,boolean state,int count){
 		this.x=x;
 		this.y=y;
 		this.state=state;
+		this.count=count;
+	}
+	Location(int x,int y){
+		this.x=x;
+		this.y=y;
 	}
 	public int getX() {
 		return x;
