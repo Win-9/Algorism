d2181a7b7487a7020f7e246a1bbd203c3918241f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-04-14 11:15:00 +0900

    Update main.java

--- BOJ/탈출2(16397)/main.java
+++ BOJ/탈출2(16397)/main.java
@@ -24,7 +24,7 @@ import java.util.Queue;
 public class bj {
 	static String message;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String[]str=br.readLine().split("");
@@ -32,20 +32,18 @@ public class bj {
 	    int N=Integer.parseInt(str[0]);//N:기본값
 	    int T=Integer.parseInt(str[1]);//T총횟수
 	    int G=Integer.parseInt(str[2]);//G:결과값
-	    
-	    boolean[]visited=new boolean[2];
-	    
-	    message=bfs(0,visited,N,T,G);
+	    	    
+	    message=bfs(0,N,T,G);
 	    System.out.println(message);
 	    
 	}
 	
-	static public String bfs(int v, boolean[]visited,int N,int T,int G) {
-		visited[v]=true;//visited[0]=>A,visited[1]=>B
+	static public String bfs(int v, int N,int T,int G) {
 		Queue<Func>queue=new LinkedList<>();
-		queue.add(new Func(visited[v],N,0));
+		queue.add(new Func(true,N,0));
+		queue.add(new Func(false,N,0));
 		Func f=null;
-		while(f.getCount()<T) {
+		do {
 			f=queue.poll();
 			if(N==G) {
 				break;
@@ -55,7 +53,7 @@ public class bj {
 			}
 			queue.add(new Func(true,N,f.count+1));
 			queue.add(new Func(false,N,f.count+1));
-		}
+		}while(f.getCount()<T) ;
 		return Integer.toString(f.count);
 	}
 }
@@ -65,30 +63,32 @@ class Func{
 	int num;
 	boolean boom;
 	Func(boolean where,int num,int count){
-		this.count=count;
-		if(where) {
-			this.num+=1;
-			return;
-		}
 		if(num*2>99999) {
 			this.boom=false;
 			return;
 		}
-		StringBuilder sb=new StringBuilder();
-		String []b=Integer.toString(num*2).split("");
-		int c=Integer.parseInt(b[0])-1;
-		if(c<=0) {
-			for(int i=1;i<b.length;i++) {
-				sb.append(b[i]);
-			}
-			this.num=Integer.parseInt(sb.toString());
+		this.count=count;
+		if(where) {//A button
+			this.num+=1;
+			return;
 		}
 		else {
-			sb.append(Integer.toString(c));
-			for(int i=1;i<b.length;i++) {
-				sb.append(b[i]);
+			StringBuilder sb=new StringBuilder();
+			String []b=Integer.toString(num*2).split("");
+			int c=Integer.parseInt(b[0])-1;
+			if(c<=0) {//B button
+				for(int i=1;i<b.length;i++) {
+					sb.append(b[i]);
+				}
+				this.num=Integer.parseInt(sb.toString());
 			}
-			this.num=Integer.parseInt(sb.toString());
+			else {
+				sb.append(Integer.toString(c));
+				for(int i=1;i<b.length;i++) {
+					sb.append(b[i]);
+				}
+				this.num=Integer.parseInt(sb.toString());
+			}
 		}
 		
 	}
