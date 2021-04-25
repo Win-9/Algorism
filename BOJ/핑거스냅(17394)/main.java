932d580c0e1de575a7ee9363f1b5d27cbcd8e7c7
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-04-25 21:56:31 +0900

    Update main.java

--- BOJ/핑거스냅(17394)/main.java
+++ BOJ/핑거스냅(17394)/main.java
@@ -15,14 +15,12 @@ public class bj {
 	    StringBuilder sb=new StringBuilder();
 	    int T=Integer.parseInt(br.readLine());
 	    
-	    /*for(int i=0;i<T;i++) {
+	    for(int i=0;i<T;i++) {
 		    String[]str=br.readLine().split(" ");
 		    sb.append(bfs(Integer.parseInt(str[0]),Integer.parseInt(str[1]),Integer.parseInt(str[2])));
 		    sb.append("\n");
-		    System.out.println("====================");
-	    }*/
-	    String[]str=br.readLine().split(" ");
-	    sb.append(bfs(Integer.parseInt(str[0]),Integer.parseInt(str[1]),Integer.parseInt(str[2])));
+	    }
+	    sb.delete(sb.length()-1, sb.length());
 	    System.out.println(sb);
 
 	}
@@ -30,16 +28,14 @@ public class bj {
 		Queue<Finger>queue=new LinkedList<>();
 		boolean[] visited=new boolean[1000000];
 		visited[N]=true;
-		queue.add(new Finger(N,0));queue.add(new Finger(N,0));
-		queue.add(new Finger(N,0));queue.add(new Finger(N,0));
+		queue.add(new Finger(N,0));
 		while(queue.size()!=0) {
 			Finger f=queue.poll();
-			System.out.println("f.number:"+f.number);
 			if(f.number>=A && f.number<=B && isPrime(f.number)) {
 				return f.count;
 			}
 			for(int i=0;i<4;i++) {
-				cases(N,i,queue,f.count,visited);
+				cases(f.number,i,queue,f.count,visited);
 			}
 		}
 		return -1;
@@ -47,32 +43,28 @@ public class bj {
 	static public void cases(int number,int option,Queue<Finger>queue,int count,boolean[]visited) {
 		if(number>0) {
 			switch(option) {
-			case 1:
+			case 0:
 				if(number/2>0 && !visited[number/2]) {
 					queue.add(new Finger(number/2,count+1));
 					visited[number/2]=true;
-					System.out.println("here1");
 				}
 				break;
-			case 2:
+			case 1:
 				if(number/3>0 && !visited[number/3]) {
 					queue.add(new Finger(number/3,count+1));
 					visited[number/3]=true;
-					System.out.println("here2");
+				}
+				break;
+			case 2:
+				if(number+1<10000 && !visited[number+1]) {
+					queue.add(new Finger(number+1,count+1));
+					visited[number+1]=true;
 				}
 				break;
 			case 3:
-				if(!visited[number+1]) {
-					queue.add(new Finger(number+1,count+1));
-					visited[number+1]=true;
-					System.out.println("here3");
-				}
-				break;
-			case 4:
 				if(number-1>0 && !visited[number-1]) {
 					queue.add(new Finger(number-1,count+1));
 					visited[number-1]=true;
-					System.out.println("here4");
 				}
 				break;
 			}
