c4a1eb28e6776de3fe549eca8b5b1146fdf73aa7
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-05-02 23:19:00 +0900

    Update main.java

--- BOJ/영역구하기(2583)/main.java
+++ BOJ/영역구하기(2583)/main.java
@@ -1,14 +1,14 @@
-package por;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class bj {
+class Main {
 	static int[] x_loc= {1,-1,0,0};
 	static int[] y_loc= {0,0,1,-1};
 	static int N;
 	static int M;
+	static int size=1;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +17,7 @@ public class bj {
 	    M=Integer.parseInt(str[0]);
 	    N=Integer.parseInt(str[1]);
 	    int K=Integer.parseInt(str[2]);
-	    int[][]map=new int[N+1][M+1];
+	    int[][]map=new int[N][M];
 	    boolean[][]visited=new boolean[N+1][M+1];
 	    for(int i=0;i<K;i++) {
 	    	str=br.readLine().split(" ");
@@ -26,11 +26,12 @@ public class bj {
 	    	area(map,x1,y1,x2,y2);
 	    }
 	    int count=0;
-	    for(int i=0;i<=N;i++) {
-	    	for(int j=0;j<=M;j++) {
+	    for(int i=0;i<N;i++) {
+	    	for(int j=0;j<M;j++) {
 	    		if(map[i][j]!=1 && !visited[i][j]) {
-	    			list.add(dfs(i,j,map,visited,0));
+	    			list.add(dfs(i,j,map,visited));
 	    			++count;
+	    			size=1;
 	    		}
 	    	}
 	    }
@@ -42,15 +43,16 @@ public class bj {
 	    }
 	   
 	}
-	static int dfs(int x,int y,int[][] map,boolean[][]visited,int size) {
+	static int dfs(int x,int y,int[][] map,boolean[][]visited) {
 		visited[x][y]=true;
 		for(int i=0;i<4;i++) {
 			int ex1=x+x_loc[i];
 			int ex2=y+y_loc[i];
 			
-			if((ex1<=N && ex1>=0) && (ex2<=M && ex2>=0) && !visited[ex1][ex2] && map[ex1][ex2]!=1) {
+			if((ex1<N && ex1>=0) && (ex2<M && ex2>=0) && map[ex1][ex2]!=1 && !visited[ex1][ex2]) {
 				visited[ex1][ex2]=true;
-				dfs(ex1,ex2,map,visited,++size);
+				dfs(ex1,ex2,map,visited);
+				++size;
 			}
 		}
 		return size;
@@ -61,7 +63,6 @@ public class bj {
 			
 			for(int j=y1;j<y2;j++) {
 				map[x1][j]=1;
-				System.out.println("x1:"+x1+" y1:"+j);
 			}
 		}
 	}
