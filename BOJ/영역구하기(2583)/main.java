b35b9b2509f39f84c2df78fabb90a480399cef2d
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-05-02 22:47:39 +0900

    Create main.java

---
+++ BOJ/영역구하기(2583)/main.java
@@ -0,0 +1,68 @@
+package por;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class bj {
+	static int[] x_loc= {1,-1,0,0};
+	static int[] y_loc= {0,0,1,-1};
+	static int N;
+	static int M;
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String[]str=br.readLine().split(" ");
+	    ArrayList<Integer>list=new ArrayList<Integer>();
+	    M=Integer.parseInt(str[0]);
+	    N=Integer.parseInt(str[1]);
+	    int K=Integer.parseInt(str[2]);
+	    int[][]map=new int[N+1][M+1];
+	    boolean[][]visited=new boolean[N+1][M+1];
+	    for(int i=0;i<K;i++) {
+	    	str=br.readLine().split(" ");
+	    	int x1=Integer.parseInt(str[0]);int y1=Integer.parseInt(str[1]);
+	    	int x2=Integer.parseInt(str[2]);int y2=Integer.parseInt(str[3]);
+	    	area(map,x1,y1,x2,y2);
+	    }
+	    int count=0;
+	    for(int i=0;i<=N;i++) {
+	    	for(int j=0;j<=M;j++) {
+	    		if(map[i][j]!=1 && !visited[i][j]) {
+	    			list.add(dfs(i,j,map,visited,0));
+	    			++count;
+	    		}
+	    	}
+	    }
+	    
+	    System.out.println(count);
+	    Collections.sort(list);
+	    for(int i=0;i<list.size();i++) {
+	    	System.out.print(list.get(i)+" ");
+	    }
+	   
+	}
+	static int dfs(int x,int y,int[][] map,boolean[][]visited,int size) {
+		visited[x][y]=true;
+		for(int i=0;i<4;i++) {
+			int ex1=x+x_loc[i];
+			int ex2=y+y_loc[i];
+			
+			if((ex1<=N && ex1>=0) && (ex2<=M && ex2>=0) && !visited[ex1][ex2] && map[ex1][ex2]!=1) {
+				visited[ex1][ex2]=true;
+				dfs(ex1,ex2,map,visited,++size);
+			}
+		}
+		return size;
+		
+	}
+	static void area(int[][]map,int x1,int y1,int x2,int y2) {
+		for(;x1<x2;x1++) {
+			
+			for(int j=y1;j<y2;j++) {
+				map[x1][j]=1;
+				System.out.println("x1:"+x1+" y1:"+j);
+			}
+		}
+	}
+}
