fb6fcc2b9b57158ab5b9ee7a55c7598be7e04864
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-05-12 11:35:11 +0900

    Update bj.java

--- BOJ/치즈(2638)/bj.java
+++ BOJ/치즈(2638)/bj.java
@@ -9,7 +9,7 @@ public class bj {
 	static int[] y_loc= {0,0,1,-1};
 	static int N;
 	static int M;
-	static int size=1;
+	static int count=1;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +21,9 @@ public class bj {
 	    for(int i=0;i<N;i++) {
 	    	for(int j=0;j<M;j++) {
 	    		if(check_outside(i, j, map)) {
-	    			dfs(i,j,map);
+	    			dfs(i,j,map,visited);
+	    			++count;
+	    			Change(map, visited);
 	    		}
 	    	}
 	    }
@@ -29,8 +31,30 @@ public class bj {
 	static public void dfs(int n,int m,int[][]map,boolean[][]visited) {
 		visited[n][m]=true;
 		while(true) {
-			
+			for(int i=0;i<4;i++) {
+				int ex1=n+x_loc[i];
+				int ex2=m+y_loc[i];
+				if (check(ex1,ex2,map,visited)){
+					visited[ex1][ex2]=true;
+					dfs(ex1,ex2,map,visited);
+				}
+			}
 		}
+	}
+	static public void Change(int[][]map,boolean[][]visited) {
+		for(int i=0;i<N;i++) {
+	    	for(int j=0;j<M;j++) {
+	    		if(visited[i][j]) {
+	    			map[i][j]=0;
+	    		}
+	    	}
+	    }
+	}
+	static public boolean check(int ex1,int ex2, int [][]map,boolean[][]visited) {
+		if((ex1>=0 && ex1<N) && (ex2>=0 && ex2<M) && check_outside(ex1, ex2, map) && !visited[ex1][ex2]) {
+			return true;
+		}
+		return false;
 	}
 	static public boolean check_outside(int n,int m,int [][]map) {
 		if( n==0 || m==0 || n==N-1 || m==M-1) {
@@ -43,7 +67,9 @@ public class bj {
 		}
 		return false;
 	}
+	
 }
+
 class Location{
 	int x;
 	int y;
