73601d16eea8edd6ce2b6100c18a6889b3c9ea4f
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-05-19 18:55:56 +0900

    Update bj.java

--- BOJ/치즈(2638)/bj.java
+++ BOJ/치즈(2638)/bj.java
@@ -1,8 +1,9 @@
 package por;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class bj {
 	static int[] x_loc= {1,-1,0,0};
@@ -15,29 +16,61 @@ public class bj {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String[]str=br.readLine().split(" ");
 	    N=Integer.parseInt(str[0]);M=Integer.parseInt(str[1]);
+	    File file=new File("C:\\Users\\LG\\ss.txt");
 	    int[][]map=new int[N][M];
 	    boolean[][]visited=new boolean[N][M];
-	    
-	    for(int i=0;i<N;i++) {
-	    	for(int j=0;j<M;j++) {
-	    		if(check_outside(i, j, map)) {
-	    			dfs(i,j,map,visited);
-	    			++count;
-	    			Change(map, visited);
-	    		}
+    	FileReader fr=new FileReader(file);
+    	br = new BufferedReader(fr);
+    	String cur="";int q=0;
+    	while((cur=br.readLine())!=null) {
+    		str=cur.split(" ");
+    		for(int j=0;j<M;j++) {
+	    		map[q][j]=Integer.parseInt(str[j]);
 	    	}
+    		q++;
+    	}
+	    
+	    /*for(int i=0;i<N;i++) {
+	    	str=br.readLine().split(" ");
+	    	for(int j=0;j<M;j++) {
+	    		map[i][j]=Integer.parseInt(str[j]);
+	    	}
+	    }*/
+	    while(true) {
+	    	boolean is_in=false;
+	    	for(int i=0;i<N;i++) {
+	    		for(int j=0;j<M;j++) {
+	    			System.out.print(map[i][j]+" ");
+	    		}
+	    		System.out.println();
+	    	}
+	    	System.out.println("===========================");
+	    	for(int i=0;i<N;i++) {
+		    	for(int j=0;j<M;j++) {
+		    		if(check_outside(i, j, map)) {
+		    			System.out.println("dfs x:"+i+" y:"+j);
+		    			dfs(i,j,map,visited);
+		    			is_in=true;
+		    		}
+		    	}
+		    }
+	    	if(is_in==false)
+	    		break;
+	    	++count;
+	    	Change(map, visited);
 	    }
+	    System.out.println("count:"+count);
 	}
+	
 	static public void dfs(int n,int m,int[][]map,boolean[][]visited) {
 		visited[n][m]=true;
-		while(true) {
-			for(int i=0;i<4;i++) {
-				int ex1=n+x_loc[i];
-				int ex2=m+y_loc[i];
-				if (check(ex1,ex2,map,visited)){
-					visited[ex1][ex2]=true;
-					dfs(ex1,ex2,map,visited);
-				}
+		System.out.println("x:"+n+" y:"+m);
+		for(int i=0;i<4;i++) {
+			int ex1=n+x_loc[i];
+			int ex2=m+y_loc[i];
+			if (check(ex1,ex2,map,visited)){//
+				visited[ex1][ex2]=true;
+				dfs(ex1,ex2,map,visited);
 			}
 		}
 	}
@@ -57,11 +90,11 @@ public class bj {
 		return false;
 	}
 	static public boolean check_outside(int n,int m,int [][]map) {
-		if( n==0 || m==0 || n==N-1 || m==M-1) {
-			return true;
-		}
-		else {
-			if(map[n-1][m]==0 || map[n][m-1]==0 || map[n][m+1]==0 || map[n-1][m]==0) {
+		if(map[n][m]==1) {
+			if( n==0 || m==0 || n==N-1 || m==M-1) {//양끝좌표가 1이면 무조건 녹음
+				return true;
+			}
+			else if(map[n-1][m]==0 || map[n][m-1]==0 || map[n][m+1]==0 || map[n-1][m]==0) {//1인것에서 십자좌표탐색
 				return true;
 			}
 		}
