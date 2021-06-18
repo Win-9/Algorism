c9ca6650415b09cd48321b7d47f995ee583d23f9
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-18 22:09:20 +0900

    Update Main.java

--- BOJ/보석도둑(1202)/Main.java
+++ BOJ/보석도둑(1202)/Main.java
@@ -2,10 +2,8 @@ package por;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.PriorityQueue;
 
 public class bj {
 	public static void main(String[] args) throws Exception {
@@ -14,33 +12,32 @@ public class bj {
 	    String[]str=br.readLine().split(" ");
 	    int N=Integer.parseInt(str[0]);
 	    int K=Integer.parseInt(str[1]);
-	    ArrayList<Jam>list=new ArrayList<>();
+	    PriorityQueue<Jam>queue=new PriorityQueue<Jam>();
 	    int[]bag=new int[K];
 	    boolean[]bag_visited=new boolean[K];
-	    int result=0;
+	    long result=0;
 	    for(int i=0;i<N;i++) {
 	    	str=br.readLine().split(" ");
-	    	list.add(new Jam(Integer.parseInt(str[0]),Integer.parseInt(str[1])));
-	    }
-	    Collections.sort(list);//V기준내림차순
+	    	queue.add(new Jam(Integer.parseInt(str[0]),Integer.parseInt(str[1])));
+	    }	    
 	    
 	    for(int i=0;i<K;i++) {
 	    	bag[i]=Integer.parseInt(br.readLine());
 	    }
+	  
 	    Arrays.sort(bag);//오름
 	    
-	    Iterator<Jam>iter=list.iterator();
-	    while(iter.hasNext()) {
-	    	Jam val=iter.next();
+	    while(!queue.isEmpty()) {
+	    	Jam val=queue.poll();
 	    	for(int i=0;i<K;i++) {
-	    		if(bag[i]>val.M && !bag_visited[i]) {
+	    		if(bag[i]>=val.M && !bag_visited[i]) {
 	    			bag_visited[i]=true;
 	    			result+=val.V;
 	    			break;
 	    		}
 	    	}
 	    }
-	    System.out.println(result);
+	    System.out.println("Result:"+result);
 	}
 }
 
@@ -56,7 +53,7 @@ class Jam implements Comparable<Jam>{
 	@Override
 	public int compareTo(Jam o) {
 		// TODO Auto-generated method stub
-		return this.M>o.M?1:-1;
+		return this.V>o.V?-1:1;
 	}
 	
 
