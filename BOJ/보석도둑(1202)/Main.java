e1a85d710ff8c1dd711567cd26a36bfe289769ed
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-23 00:26:46 +0900

    Update Main.java

--- BOJ/보석도둑(1202)/Main.java
+++ BOJ/보석도둑(1202)/Main.java
@@ -1,43 +1,43 @@
-package por;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-public class bj {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String[]str=br.readLine().split(" ");
 	    int N=Integer.parseInt(str[0]);
 	    int K=Integer.parseInt(str[1]);
-	    PriorityQueue<Jam>queue=new PriorityQueue<Jam>();
+	    LinkedList<Jam>list=new LinkedList<>();
 	    int[]bag=new int[K];
 	    boolean[]bag_visited=new boolean[K];
 	    long result=0;
 	    for(int i=0;i<N;i++) {
 	    	str=br.readLine().split(" ");
-	    	queue.add(new Jam(Integer.parseInt(str[0]),Integer.parseInt(str[1])));
-	    }	    
-	    
+	    	list.add(new Jam(Integer.parseInt(str[0]),Integer.parseInt(str[1])));
+	    }	  
+	    Collections.sort(list);
 	    for(int i=0;i<K;i++) {
 	    	bag[i]=Integer.parseInt(br.readLine());
 	    }
-	  
 	    Arrays.sort(bag);//오름
 	    
-	    while(!queue.isEmpty()) {
-	    	Jam val=queue.poll();
-	    	for(int i=0;i<K;i++) {
-	    		if(bag[i]>=val.M && !bag_visited[i]) {
-	    			bag_visited[i]=true;
-	    			result+=val.V;
+	    Iterator<Jam>iter=list.iterator();
+	    while(iter.hasNext()) {
+	    	Jam jam=iter.next();
+	    	for(int j=0;j<bag.length;j++) {
+	    		if(!bag_visited[j] && bag[j]>=jam.M) {
+	    			bag_visited[j]=true;
+	    			result+=jam.V;
 	    			break;
 	    		}
 	    	}
 	    }
-	    System.out.println("Result:"+result);
+	    System.out.println(result);
 	}
 }
 
