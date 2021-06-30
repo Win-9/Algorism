dfde2678bf0c0832cc17e144f2f18487a9153c39
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-06-30 23:29:30 +0900

    Update Main.java

--- BOJ/강의실배정(11000)/Main.java
+++ BOJ/강의실배정(11000)/Main.java
@@ -1,58 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    LinkedList<Group>list=new LinkedList<Group>();
+	    PriorityQueue<Integer>result_queue=new PriorityQueue<Integer>();
 	    int result=0;
 	    int N=Integer.parseInt(br.readLine());
-	    boolean[]visited=new boolean[N];
+	    int[][]data=new int[N][2];
 	    for(int i=0;i<N;i++) {
 	    	String[]str=br.readLine().split(" ");
-	    	list.add(new Group(Integer.parseInt(str[0]),Integer.parseInt(str[1]),i));
-	    }  
-	    Collections.sort(list);
-	    for(Group g1:list) {
-	    	if(visited[g1.num]) {continue;}
-	    	visited[g1.num]=true;
-	    	result++;
-	    	Iterator<Group>iter=list.iterator();
-	    			    
-	    	while(iter.hasNext()) {
-	    		Group g2=iter.next();
-	    		if(!visited[g2.num] && g1.T==g2.S) {
-	    			visited[g2.num]=true;
-	    			g1=g2;
-	    			continue;
-	    		}
+	    	data[i][0]=Integer.parseInt(str[0]);
+	    	data[i][1]=Integer.parseInt(str[1]);
+	    }
+	    Arrays.sort(data, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				return o1[0]>o2[0]?1:-1;
+			} 
+		});
+	    result_queue.add(data[0][1]);
+	    
+	    for(int i=1;i<N;i++) {
+	    	
+	    	if(result_queue.peek()==data[i][0]) {
+	    		result_queue.poll();
 	    	}
+	    	result_queue.add(data[1][1]);
 	    }
 	    
-	    System.out.println(result);
+	    System.out.println(result_queue.size());
 	    
 	}
-}
-
-class Group implements Comparable<Group>{
-	int S;
-	int T;
-	int num;
-	public Group(int S,int T,int num) {
-		// TODO Auto-generated constructor stub
-		this.S=S;
-		this.T=T;
-		this.num=num;
-	}
-	
-	@Override
-	public int compareTo(Group o) {
-		// TODO Auto-generated method stub
-		return this.S>o.S?1:-1;
-	}
-
 }
