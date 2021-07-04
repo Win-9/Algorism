83562b12aa1a3b218cb2e1d7f10ab8203d8dabd5
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-04 22:38:06 +0900

    Update Main.java

--- BOJ/회문(17609)/Main.java
+++ BOJ/회문(17609)/Main.java
@@ -1,10 +1,9 @@
-package por;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Stack;
 
-public class bj {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,14 +14,34 @@ public class bj {
 	    	all_str[i]=br.readLine();
 	    }
 	    
-	    
-	    
-	}
-	public static void Check(String str) {
-	    Stack<Character>stack=new Stack<Character>();
-	    for(int i=0;i<str.length();i++) {
-	    	stack.push(str.charAt(i));
-	    	
+	    for(int i=0;i<N;i++) {
+		    System.out.println(Check(all_str[i]));
 	    }
+	    
 	}
+	
+	public static int Check(String str) {
+	    Stack<Character>stack=new Stack<Character>();
+	    ArrayList<Character>list=new ArrayList<Character>();
+	    int i=0;
+	    for(;i<str.length();i++) {
+	    	if(stack.size()!=0 && str.charAt(i)==(stack.peek())) {
+	    		break;
+	    	}
+	    	stack.push(str.charAt(i));
+	    }
+	    
+	    for(;i<str.length();i++) {
+	    	if(str.charAt(i)==stack.peek()) {
+	    		stack.pop();
+	    		continue;
+	    	}
+	    	list.add(str.charAt(i));
+	    }
+	    
+	    return list.size()==0 && stack.size()==0 ? 0: (list.size()==1 && stack.size()==0 ) || (list.size()==0 && stack.size()==1 )?1:2;
+	    
+	    
+	}
+	
 }
