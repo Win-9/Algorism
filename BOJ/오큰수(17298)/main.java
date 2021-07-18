531f68da43c03e1b65cbf8638f7c1193e3ef6cd0
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-19 00:54:57 +0900

    Update main.java

--- BOJ/오큰수(17298)/main.java
+++ BOJ/오큰수(17298)/main.java
@@ -1,35 +1,41 @@
+package por;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.ArrayList;
 
-public class Main {
+public class bj {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringBuilder sb=new StringBuilder();
 	    int N=Integer.parseInt(br.readLine());
+	    int[]arr=new int[N];
+	    
 	    String[]str=br.readLine().split(" ");
-	    int top=0;
-	    int idx=0;
-	    while(idx!=(N-1)) {
-	    	if(top>N-2) {
-	    		sb.append("-1\n");
-	    		++idx;
-	    		top=idx;
-	    		continue;
-	    	}
-	    	else if(Integer.parseInt(str[idx])<(Integer.parseInt(str[++top]))) {
-	    		sb.append(str[top]+"\n");
-	    		++idx;
-	    		top=idx;
-	    		continue;
-	    	}
-	    		    	
+	    for(int i=0;i<N;i++) {
+	    	arr[i]=Integer.parseInt(str[i]);
 	    }
-	    sb.append("-1");
-	    System.out.println(sb);
-
+	    
+	    Stack<Integer>stack=new Stack<Integer>();
+	    
+	    for(int i=0;i<N;i++) {
+	    	
+	    	while(!stack.empty() && arr[stack.peek()]<arr[i]) {
+	    		arr[stack.pop()]=arr[i];
+	    	}
+	    	stack.push(i);
+	    	
+	    }
+	    
+	    while(!stack.isEmpty()) {
+			arr[stack.pop()]=-1;
+		}
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i]+" ");
+		}
+		System.out.println(sb.toString());
 		
 	}
 	
