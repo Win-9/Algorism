f9ef9e6751f4c949043acf295aca388ba2a6e84b
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-19 01:12:08 +0900

    Update main.java

--- BOJ/오큰수(17298)/main.java
+++ BOJ/오큰수(17298)/main.java
@@ -3,7 +3,7 @@ package por;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class bj {
 	public static void main(String[] args) throws Exception {
@@ -11,12 +11,11 @@ public class bj {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringBuilder sb=new StringBuilder();
 	    int N=Integer.parseInt(br.readLine());
-	    int[]arr=new int[N];
+	    
 	    
 	    String[]str=br.readLine().split(" ");
-	    for(int i=0;i<N;i++) {
-	    	arr[i]=Integer.parseInt(str[i]);
-	    }
+	    
+	    int[]arr =Stream.of(str).mapToInt(s->Integer.parseInt(s)).toArray();
 	    
 	    Stack<Integer>stack=new Stack<Integer>();
 	    
