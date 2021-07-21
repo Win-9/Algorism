71b9a54782f76955bdd03ad6d171636ff3317309
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-07-21 22:41:48 +0900

    Update main.java

--- BOJ/프린터큐/main.java
+++ BOJ/프린터큐/main.java
@@ -3,7 +3,9 @@ package por;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Stream;
 
 public class bj {
@@ -16,28 +18,40 @@ public class bj {
 	    	str=br.readLine().split(" ");
 	    	int N=Integer.parseInt(str[0]);
 	    	int M=Integer.parseInt(str[1]);
-	    	Check(N,M,br.readLine());
+	    	Check(N,M,br.readLine().split(" "));
 	    	
 	    }
 		
 	}
 	
-	public static void Check(int N,int M,String str) {
-		int[]arr=Stream.of(str).mapToInt(s->Integer.parseInt(s)).toArray();
-		int max=Arrays.stream(arr).max().getAsInt();
-		int count=0;int i=0;
-		while(true) {
-			if(arr[i]<max) {
-				int temp=arr[i];
-				arr[i]=arr[arr.length-1];
-				arr[arr.length-1]=temp;
-				count++;
-			}
-			else {
-				
-				
-			}
+	public static void Check(int N,int M,String []str) {
+		System.out.println("N:"+N+" M:"+M);
+		M=Integer.parseInt(str[M]);
+		/*int[]arr=Stream.of(str).mapToInt(s->Integer.parseInt(s)).toArray();
+		Queue<Integer>queue=new LinkedList<>(Arrays.asList(arr));
+		*/
+		
+		Queue<Integer>queue=new LinkedList<>();
+		for(String s:str) {
+			queue.add(Integer.parseInt(s));
 		}
+		int max=Collections.max(queue);
+
+		int count=1;
+		while(!(queue.peek()>max && queue.peek()==M)) {
+			System.out.println("peek():"+queue.peek());
+			if(queue.peek()==max) {
+				count++;
+				System.out.println("poll:"+queue.poll()+"===================");
+				max=Collections.max(queue);
+				System.out.println("changed max:"+max);
+				continue;
+			}
+			
+			queue.add(queue.poll());
+		}
+		
+		System.out.println(count);
 		
 	}
 	
