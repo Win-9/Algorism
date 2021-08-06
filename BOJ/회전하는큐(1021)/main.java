18c96df87db97f46e4094916094badfdcf862971
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-08-07 02:21:08 +0900

    Update main.java

--- BOJ/회전하는큐(1021)/main.java
+++ BOJ/회전하는큐(1021)/main.java
@@ -1,5 +1,3 @@
-package por;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -9,12 +7,14 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class bj {
+public class Main {
+	static int count=0;
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String[]str=br.readLine().split(" ");
-	    Deque<Integer>queue=new LinkedList<>();
+	    ArrayList<Integer>queue=new ArrayList<>();
 	    int N=Integer.parseInt(str[0]);
 	    int M=Integer.parseInt(str[1]);
 	    int[]arr=Stream.of(br.readLine().split(" "))
@@ -23,43 +23,28 @@ public class bj {
 
 	    int idx=0;
 
-	    int count=0;//2,9,5
-    	for(int i=N;i>0;i--) {
+    	for(int i=1;i<=N;i++) {
     		queue.add(i);
-    	}
-    	print(queue);
- 
+    	} 
     	while(idx!=arr.length) {
-    		if(arr[idx]==queue.peekLast()) {
-    			queue.pollLast();
+
+    		if(arr[idx]==queue.get(0)) {
+    			queue.remove(0);
     			++idx;
-    			print(queue);
     			continue;
     		}
 
-    		if(arr[idx]<queue.size()/2) {
-    			queue.addFirst((queue.pollLast()));
+    		if(queue.indexOf(arr[idx])<=queue.size()/2) {
+    			queue.add(queue.remove(0));
     			count++;
-    			print(queue);
-
     		}
-    		else if(arr[idx]>=queue.size()/2) {
-    			queue.addLast((queue.pollFirst()));
+    		
+    		else {
+    			queue.add(0,queue.remove(queue.size()-1));
     			count++;
-    			print(queue);
-
     		}
     	}
-    	
     	System.out.println(count);
 	}
-	
-	public static void print(Deque<Integer>queue) {
-		for(int i:queue) {
-			System.out.print(i+" ");
-		}
-		System.out.println();
-	}
-
 	
 }
