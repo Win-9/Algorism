dbfd7f7bf5bfed90bb564454ba7207055ebfaf3d
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-08-18 02:16:32 +0900

    Update main.java

--- BOJ/AC(5430)/main.java
+++ BOJ/AC(5430)/main.java
@@ -1,5 +1,3 @@
-package por;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -10,38 +8,29 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
-public class bj {
+public class Main {
 	static int count=0;
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int T=Integer.parseInt(br.readLine());
-	    StringTokenizer st;
-	    StringBuilder sb=new StringBuilder();
-	    StringBuilder ex;//[1,2,3,4]
+
 	    for(int i=0;i<T;i++) {
+		    StringBuilder sb=new StringBuilder();
 		    String str=br.readLine();
 		    int N=Integer.parseInt(br.readLine());
-		    st=new StringTokenizer(br.readLine(),"[]");
-		    Stream.of(st).filter(s->!s.equals(",")).forEach(s->sb.append(s));
+		    String temp = br.readLine().replace("[", "").replace("]", "").replace(",", " ");
+			StringTokenizer st = new StringTokenizer(temp);
 		    
-		    System.out.println("sb:"+sb);
+		    while(st.hasMoreTokens()) {
+		    	sb.append(st.nextToken());
+		    }
+		    		    
 		    func1(sb,str);
 		    print(sb);
 	    }	
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st=new StringTokenizer(br.readLine(),"[]");
-		System.out.println("st:"+st);
-		
-	    StringBuilder sb=new StringBuilder();
-	    for(int i=0;i<4;i++) {
-	    	sb.append(Integer.parseInt(st.nextToken()));
-	    }
 	    
-		
-	    System.out.println(sb);
 	}
 	
 	public static void func1(StringBuilder sb,String str) {
@@ -52,7 +41,7 @@ public class bj {
 				sb.reverse();
 				break;
 			case 'D':
-				if(sb.equals("")) {
+				if((sb.toString()).equals("")) {
 					break Loop1;
 				}
 				sb.delete(0,1);
@@ -62,7 +51,7 @@ public class bj {
 	}
 	
 	public static void print(StringBuilder sb) {
-		if(sb.equals("")) {
+		if((sb.toString()).equals("")) {
 			System.out.println("error");
 		}
 		else {
