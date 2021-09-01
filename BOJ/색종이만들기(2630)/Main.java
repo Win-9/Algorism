7cb9bbc4ff6809e384ed60a47efe1e398fdc1f2a
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-09-01 21:07:45 +0900

    Update Main.java

--- BOJ/색종이만들기(2630)/Main.java
+++ BOJ/색종이만들기(2630)/Main.java
@@ -2,57 +2,64 @@ package por;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 public class bj {
+	static int[][]map;
 	static int white=0;
 	static int blue=0;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int N=Integer.parseInt(br.readLine());
-	    int[][]map=new int[N][N];
-	    for(int i=0;i<N;i++) {
-	    	String []str=br.readLine().split(" ");
-	    	for(int j=0;j<N;j++) {
+	    map=new int[N][N];
+	    for(int i=0;i<8;i++) {
+	    	String[]str=br.readLine().split(" ");
+	    	for(int j=0;j<8;j++) {
 	    		map[i][j]=Integer.parseInt(str[j]);
 	    	}
 	    }
-	    
-	    Find(map,0,N);
-	    
-	}
-	public static void Find(int[][]map,int low,int high) {
-		int middle,left,right;
-		boolean check_left=true;
-		boolean check_right=true;
-		middle=(low+high)/2;
-		Loop1:
-		for(int i=0;i<middle;i++) {
-			for(int j=0;j<middle;j++) {
-				int save=map[i][j];
-				if(map[i][j]!=save) {
-					check_left=false;
-					break Loop1;
-				}
-			}
-		}
-		if(check_left==false) {
-			Find(map,low,middle);
-		}
-		
-		Loop2:
-		for(int i=middle;i<high;i++) {
-			for(int j=middle;j<high;j++) {
-				int save=map[i][j];
-				if(map[i][j]!=save) {
-					check_left=false;
-					break Loop2;
-				}
-			}
-		}
-		if(check_left==false) {
-			Find(map,middle,high);
-		}
+	    merge(0,0,N);
+	    System.out.print(white+"\n"+blue);
 	}
 	
+	public static void merge(int x,int y,int N) {
+		int sum=0;
+		
+		if(N==1) {
+			if(map[x][y]==1) {
+				blue++;
+			}
+			else {
+				white++;
+			}
+			return;
+		}
+		
+		for(int i=x;i<x+N;i++) {
+			for(int j=y;j<y+N;j++) {
+				sum+=map[i][j];
+			}
+		}
+		
+		if(sum==N*N) {
+			blue++;
+		}
+		else if(sum==0) {
+			white++;
+		}
+		else {
+			merge(x,y,N/2);
+			merge(x+N/2,y,N/2);
+			merge(x,y+N/2,N/2);
+			merge(x+N/2,y+N/2,N/2);
+		}
+	}
 }
