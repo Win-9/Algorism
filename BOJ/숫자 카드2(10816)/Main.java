7036566f2a5f8d6f4021c70456d12c4136902388
Win-9 <80390524+Win-9@users.noreply.github.com>
2022-01-06 15:56:52 +0900

    Update Main.java

--- BOJ/숫자 카드2(10816)/Main.java
+++ BOJ/숫자 카드2(10816)/Main.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		String[] n = br.readLine().split(" ");
 		int[] nArr = Arrays.stream(n)
@@ -22,47 +22,46 @@ public class Main {
 				.mapToInt(s -> Integer.parseInt(s)).toArray();
 		
 		for (int i = 0; i < M; i++) {
-			int index = binarySearch(nArr, mArr[i]);
-			if(index == -1) {
-				System.out.print(0+" ");
+			sb.append(upperBound(nArr, mArr[i])-
+					lowerBound(nArr, mArr[i])+" ");
+		}
+		System.out.println(sb);
+	}
+
+
+	static int lowerBound(int[] arr, int key) {
+		int lo = 0; 
+		int hi = arr.length; 
+	 
+		while (lo < hi) {
+	 
+			int mid = (lo + hi) / 2;
+			if (key <= arr[mid]) {
+				hi = mid;
 				continue;
 			}
-			System.out.print(findSameIndex(nArr, nArr[index] , index)+" ");
+			lo = mid + 1;
+	 
 		}
+		return lo;
 	}
-	
-	static int findSameIndex(int[]arr, int key, int index) {
-		int sum = 0;
-		int pr = index;
-		int pl = index;
-		
-		while(pl > -1 && arr[pl--] == key) {
-			sum++;
-		}
-		
-		while( pr < arr.length && arr[pr++] == key) {
-			sum++;
-		}
-		
-		return sum-1;
-	}
-
-	static int binarySearch(int[] arr, int key) {
-		int pl = 0;
-		int pr = arr.length - 1;
-		while (pl <= pr) {
-			int pc = (pl + pr) / 2;
-			if (arr[pc] == key) {
-				return pc;
+	 
+	static int upperBound(int[] arr, int key) {
+		int lo = 0; 
+		int hi = arr.length; 
+	 
+		while (lo < hi) {
+	 
+			int mid = (lo + hi) / 2;
+	 
+			// key값이 중간 위치의 값보다 작을 경우
+			if (key < arr[mid]) {
+				hi = mid;
+				continue;
 			}
-
-			if (arr[pc] < key) {
-				pl = pc + 1;
-			} else if (arr[pc] > key) {
-				pr = pc - 1;
-			}
+			// 중복원소의 경우 else에서 처리된다.
+			lo = mid + 1;
 		}
-		
-		return -1;
+		return lo;
 	}
 }
