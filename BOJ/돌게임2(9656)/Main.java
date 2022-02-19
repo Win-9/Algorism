36d349aff291cb474c67f70f0cd4accebb247441
Win-9 <80390524+Win-9@users.noreply.github.com>
2022-02-19 14:40:31 +0000

    Create Main.java

---
+++ BOJ/돌게임2(9656)/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if (N % 2 == 0) {
+			System.out.println("SK");
+		}
+		System.out.println("CY");
+		
+
+	}
+
+}
