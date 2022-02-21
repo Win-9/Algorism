c3197596d00d5c631a5c02e9064246948eeefa65
Win-9 <80390524+Win-9@users.noreply.github.com>
2022-02-21 16:12:04 +0000

    Create Main.java

---
+++ BOJ/이친수(2193)/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static LinkedList<Integer> list = new LinkedList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		long[]arr = new long[N + 1];
+		
+		if (N == 1) {
+			System.out.println(1);
+			return;
+		}
+		arr[1] = 1;
+		arr[2] = 1;
+		
+		
+		for(int i = 3; i <= N; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+		
+		System.out.println(arr[N]);
+	}
+
+}
