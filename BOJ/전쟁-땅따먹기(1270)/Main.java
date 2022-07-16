3d39e8a9446f1964abeb66ca96163fce6ea6a792
Win-9 <80390524+Win-9@users.noreply.github.com>
2022-07-16 07:41:38 +0000

    Update Main.java

--- BOJ/전쟁-땅따먹기(1270)/Main.java
+++ BOJ/전쟁-땅따먹기(1270)/Main.java
@@ -5,27 +5,29 @@ class Main {
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < N; i++) {
+		long N = Long.parseUnsignedLong(br.readLine());
+		
+		for(long i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			HashMap<Integer, Integer> map = new HashMap<>();
-			int max = Integer.parseInt(st.nextToken()); // 맨앞의 하나 빼줌
+			HashMap<Long, Long> map = new HashMap<>();
+			long max = Long.parseUnsignedLong(st.nextToken()); // 맨앞의 하나 빼줌
 			
 			while(st.hasMoreTokens()) {
-				int num = Integer.parseInt(st.nextToken());
-				map.put(num, map.getOrDefault(num, 0) + 1);
+				long num = Long.parseUnsignedLong(st.nextToken());
+				map.put(num, map.getOrDefault(num, (long) 0) + 1);
 			}
 			func(max, map);
+			System.gc();
 		}
 
 	}
 	
-	static void func(int num, HashMap<Integer, Integer> map) {
-		int half = num/2;
+	static void func(long num, HashMap<Long, Long> map) {
+		long half = num/2;
 				
-		for (int i : map.keySet()) {
-			int m = map.get(i);
+		for (long i : map.keySet()) {
+			long m = map.get(i);
 			if (half < m) {
 				System.out.println(i);
 				return;
