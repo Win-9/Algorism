bed7f4a6d7750966c21399be02a1bbdcce0f2b79
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-07-04 23:52:01 +0900

    Update Solution.java

--- programers/호텔대실/Solution.java
+++ programers/호텔대실/Solution.java
@@ -2,30 +2,27 @@ import java.util.*;
 import java.lang.*;
 
 class Solution {
+    static final int CLEANTIME = 10;
+    static final int HOUR = 60;
+    static final int MAXTIME = 1450;
     public int solution(String[][] book_time) {
         int answer = 1;
-        HashMap<String, String> map = new HashMap<>();
-        
+        int[] hours = new int[MAXTIME];
         for(int i = 0; i < book_time.length; i++) {
-            String startTime = book_time[i][0];
-            String endTime = book_time[i][1];
-            if(check(map, startTime, endTime)) {
-                answer++;
-            }
-            map.put(startTime, endTime);
+            String[] startTime = book_time[i][0].split(":");
+            String[] endTime = book_time[i][1].split(":");
+            
+            hours[Integer.parseInt(startTime[0]) * HOUR + Integer.parseInt(startTime[1])] += 1;
+            hours[Integer.parseInt(endTime[0]) * HOUR + Integer.parseInt(endTime[1]) + CLEANTIME] -= 1;
         }
+        
+        //누적합 구하기
+        for(int i = 1; i < MAXTIME; i++) {
+            hours[i] += hours[i - 1];
+            answer = Math.max(answer, hours[i]);
+        }
+        
         
         return answer;
-    }
-    
-    static boolean check(HashMap<String, String> map, String startTime, String endTime) {
-        for(String s : map.keySet()) {
-        if ((s.compareTo(startTime) <= 0 && (map.get(s)).compareTo(startTime) >= 0) || (s.compareTo(endTime) <= 0 && (map.get(s)).compareTo(endTime) >= 0)) {
-
-                return true;
-            }
-        }
-        
-        return false;
     }
 }
