5a36d99f96efe8c5ad0cdd7c58afd231cb06d6ba
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-08-09 16:31:49 +0900

    Update Solution.java

--- programers/[3차] 압축/Solution.java
+++ programers/[3차] 압축/Solution.java
@@ -1,49 +1,39 @@
-package com.backend.nutt.common;
-
+import java.lang.*;
 import java.util.*;
 
-public class ex {
+class Solution {
     static int index = 1;
     static Map<String, Integer> map = new HashMap<>();
-    public static void main(String[] args) {
-        String msg = "KAKAO"; // n:21
+    public int[] solution(String msg) {
         init();
         List<Integer> list = new ArrayList<>();
-        int i = 0;
-
-        for(; i < msg.length(); i++) {
+        
+        Loop1:
+        for(int i = 0; i < msg.length(); i++) {
             int j = i + 1;
-            for(; j < msg.length(); j++) {
+            for(; j <= msg.length(); j++) {
                 String str = msg.substring(i, j);
 
-                if (map.get(str) != null && j == msg.length() - 1) {
-                    list.add(map.get(msg.substring(i, j + 1)));
-                    break;
-                }
-
                 if (map.get(str) == null) {
-                    System.out.println("i = " + i + " j: " + j + " str: " + str);
                     list.add(map.get(msg.substring(i, j - 1)));
                     map.put(str, index++);
                     i = j - 2;
-                    System.out.println("i = " + i);
                     break;
+                } else if (map.get(str) != null && j == msg.length()) {
+                    list.add(map.get(msg.substring(i, j)));
+                    break Loop1;
                 }
 
             }
         }
-
-        for (Integer integer : list) {
-            System.out.print(integer + " ");
-        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
-
+    
     static void init() {
         for(int i = 65; i <= 90; i++) {
             map.put(String.valueOf((char)i), index);
             index++;
         }
-        System.out.println("map = " + map);
     }
-
 }
