d3a6af9bcb14bef9b9154d32b64dc1c40a39ec31
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-08-13 15:35:21 +0900

    Update Solution.java

--- programers/삼총사/Solution.java
+++ programers/삼총사/Solution.java
@@ -2,34 +2,32 @@ class Solution {
     static int answer = 0;
     public int solution(int[] number) {
         boolean[] visited = new boolean[number.length];
-        check(number, 0, visited, 0, 0);
+        check(number,  visited, 0, number.length, 3, 0);
+
         return answer;
     }
-    
-    static void check(int[] number, int start, boolean[] visited, int len, int count) {
-        if (len == 3) {
+
+    static void check(int[] arr, boolean[] visited, int start, int n, int r, int count) {
+        if (r == 0) {
             if (count == 0) {
                 answer++;
             }
-            
             return;
         }
-        
-        for(int i = start; i < number.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                check(number, start + 1, visited, len + 1, count + number[i]);
-                visited[i] = false;
-            }
+
+        for (int i = start; i < n; i++) {
+            visited[i] = true;
+            check(arr, visited, i + 1, n, r - 1, count + arr[i]);
+            visited[i] = false;
         }
     }
-    
+
     static void print(boolean[] visited, int[] number) {
-        for(int i = 0; i < visited.length; i++) {
+        for (int i = 0; i < visited.length; i++) {
             if (visited[i]) {
                 System.out.print(number[i] + " ");
             }
         }
-            System.out.println();
+        System.out.println();
     }
 }
