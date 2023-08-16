29acf360c4cfec29207db8137436d29ba19f2e1a
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-08-16 16:55:59 +0900

    Create  Solution.java

---
+++ programers/약수의 개수와 덧셈/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <= right; i++) {
+            int count = getDivisorCount(i);
+            if (count % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+    
+    static int getDivisorCount(int num) {
+        int count = 0;
+        for(int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
