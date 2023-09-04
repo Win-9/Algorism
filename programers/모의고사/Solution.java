08d386abcd2f6cd2389ebb63c0de47036b8503de
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-09-04 22:20:46 +0900

    Update Solution.java

--- programers/모의고사/Solution.java
+++ programers/모의고사/Solution.java
@@ -7,56 +7,37 @@ class Solution {
     static int[] thirdPeople ={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     public int[] solution(int[] answers) {
         List<Integer> list = new ArrayList<>();
-        int first = 0;
-        int second = 0;
-        int third = 0;
+        int[] score = new int[3];
         
         for(int i = 0; i < answers.length; i++) {
             if (firstPeople[i % firstPeople.length] == answers[i]) {
-                first++;
+                score[0]++;
             } 
             
             if (secondPeople[i % secondPeople.length] == answers[i]) {
-                second++;
+                score[1]++;
             }
             
             if (thirdPeople[i % thirdPeople.length] == answers[i]) {
-                third++;
+                score[2]++;
             }
         }
         
-        if (first == second && second == third) {
-            list.add(1);
-            list.add(2);
-            list.add(3);
-        } else if (first >= second || first < second) {
-            if (first > second) {
-                list.add(1);
-            } else if (first < second) {
-                list.add(2);
-            } else {
-                list.add(1);
-                list.add(2);
-            }
-        } else if (second >= third || second < third) {
-            if (second > third) {
-                list.add(2);
-            } else if (second < third) {
-                list.add(3);
-            } else {
-                list.add(2);
-                list.add(3);
-            }
-        } else if (first >= third || first < third) {
-            if (first > third) {
-                list.add(1);
-            } else if (first < third) {
-                list.add(3);
-            } else {
-                list.add(1);
-                list.add(3);
+        list.add(1);
+        
+        for(int i = 1; i < 3; i++) {
+            if (score[i] > score[list.get(0) - 1]) {
+                if (list.size() == 2) {
+                    list.clear();
+                } else {
+                    list.remove(0);
+                }
+                list.add(i + 1);
+            } else if (score[i] == score[list.get(0) - 1]) {
+                list.add(i + 1);
             }
         }
+        
         
         
         return list.stream().mapToInt(a -> a).toArray();
