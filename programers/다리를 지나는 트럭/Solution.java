4ebc1ddfb3c41e1080f97a3af0bc1a473d5083ad
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-09-13 22:30:34 +0900

    Update Solution.java

--- programers/다리를 지나는 트럭/Solution.java
+++ programers/다리를 지나는 트럭/Solution.java
@@ -5,28 +5,30 @@ class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         Queue<Integer> queue = new LinkedList<>();
-        int sum = 0;
+        int nowWeight = 0;
+        
         for(int i = 0; i < truck_weights.length; i++) {
-            int truck = truck_weights[i];
-            if (queue.isEmpty() || sum + truck <= weight) { //다리가 비고 무게 충당 가능할 때
+            System.out.println("i: " + truck_weights[i] + " " + answer);
+            if (queue.size() <=  bridge_length 
+                && (nowWeight + truck_weights[i]) <= weight) {
+                queue.add(truck_weights[i]);
+                nowWeight += truck_weights[i];
                 answer++;
-                sum += truck;
-                queue.add(truck);
-                continue;
-            }
-            
-            while(true) {
-                if (queue.size() == bridge_length) { // 다리 다찼을때
-                    sum -= queue.poll();
-                    break;
-                }
-                
-                if (sum + truck > weight) { // 무게 초과시
-                    queue.add(0);
+            } else {
+                System.out.println("농ㅋㅋ");
+                while(!queue.isEmpty()) {
+                    nowWeight -= queue.poll();
                     answer++;
                 }
+                
+                i--;
             }
         }
+        
+        if (!queue.isEmpty()) {
+            answer += bridge_length;
+        }
+        
         return answer;
     }
 }
