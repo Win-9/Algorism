b844a9c63a3496645803b804205fa5253c41733f
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-10-10 23:44:08 +0900

    Update Solution.java

--- programers/줄 서는 방법/Solution.java
+++ programers/줄 서는 방법/Solution.java
@@ -2,43 +2,26 @@ import java.lang.*;
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static int count = 0;
     public int[] solution(int n, long k) {
-        int[] answer = {};
-        int[] arr = new int[n];
         int[] data = new int[n];
-        visited = new boolean[n];
-        
+        int[] result = new int[n];
+        int index = 0;
+        int fact = 1;
         for(int i = 0; i < n; i++) {
             data[i] = i + 1;
+            fact *= (i + 1);
         }
         
-        arr = trace(arr, n, data, 0, k);
+        k--;
         
-        return arr;
-    }
-    
-    static int[] trace(int[] arr, int n, int[] data, int depth, long k)  {
-        
-        if (depth == n) {
-            count++;
-            return null;
+        while(n > 0) {
+            fact /= n;
+            int value = (int) (k / fact);
+            result[index++] = data[value];
+            k %= fact;
+            n--;
         }
         
-        for(int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = data[i];
-                trace(arr, n, data, depth + 1, k);
-                visited[i] = false;
-            }
-            
-            if (count == k) {
-                break;
-            }
-        }
-        
-        return arr;
+        return result;
     }
 }
