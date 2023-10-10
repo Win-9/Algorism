02fe8deed4fac28d7871b1833c95215c9540044e
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-10-11 02:01:16 +0900

    Update Solution.java

--- programers/줄 서는 방법/Solution.java
+++ programers/줄 서는 방법/Solution.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, long k) {
-        int[] data = new int[n];
+        List<Integer> data = new ArrayList<>();
         int[] result = new int[n];
         int index = 0;
         int fact = 1;
         for(int i = 0; i < n; i++) {
-            data[i] = i + 1;
+            data.add(i + 1);
             fact *= (i + 1);
         }
         
@@ -17,7 +17,8 @@ class Solution {
         while(n > 0) {
             fact /= n;
             int value = (int) (k / fact);
-            result[index++] = data[value];
+            result[index++] = data.get(value);
+            data.remove(value);
             k %= fact;
             n--;
         }
