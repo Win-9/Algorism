66f34aa96cb8bb4a8eb1427cf751f1cc63c612d1
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-11-10 23:32:28 +0900

    Create Solution.java

---
+++ programers/숫자의 표현/Solution.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            int sum = i;
+            for (int j = i + 1; j < n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                
+                if (sum + (j + 1) > n) {
+                    break;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
