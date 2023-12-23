456d6c48e130789a438e3b8d0e4ce5f8c7723241
Win-9 <80390524+Win-9@users.noreply.github.com>
2023-12-23 21:09:14 +0900

    Update Main.java

--- BOJ/싸이버개강총회/Main.java
+++ BOJ/싸이버개강총회/Main.java
@@ -8,26 +8,20 @@ class Main {
         String input = br.readLine();
         String[] str = input.split(" ");
         Map<String, Integer> map = new HashMap<>();
-        int startHour = Integer.parseInt(str[0].split(":")[0]);
-        int startMinute = Integer.parseInt(str[0].split(":")[1]);
-
-        int endHour = Integer.parseInt(str[1].split(":")[0]);
-        int endMinute = Integer.parseInt(str[1].split(":")[1]);
-
-        int streamEndHour = Integer.parseInt(str[2].split(":")[0]);
-        int streamEndMinute = Integer.parseInt(str[2].split(":")[1]);
+        String start = str[0];
+        String end = str[1];
+        String streamEnd = str[2];
 
         while((input = br.readLine()) != null && !input.equals("")) {
             str = input.split(" ");
-            int hour = Integer.parseInt(str[0].split(":")[0]);
-            int minute = Integer.parseInt(str[0].split(":")[1]);
+            String time = str[0];
 
-            if ((hour < startHour) || ((hour == startHour) && (minute == startMinute))) {
+            if (time.compareTo(start) <= 0) {
                 map.put(str[1], 1);
+                continue;
             }
 
-            if (((hour > endHour) || (hour == endHour) && (minute >= endMinute))
-                    && ((hour < streamEndHour) || (hour == streamEndHour) && (minute <= streamEndMinute))) {
+            if (time.compareTo(end) >= 0 && time.compareTo(streamEnd) <= 0) {
                 if (map.get(str[1]) != null) {
                     map.put(str[1], map.get(str[1]) - 1);
                 }
